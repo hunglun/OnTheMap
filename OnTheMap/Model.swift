daccf1b6b4b43bc47252072e967662e0ada9a80f
hunglun <hunglun.o@gmail.com>
2016-01-23 19:28:01 +0800

    The table is sorted in order of most recent to oldest update

--- OnTheMap/Model.swift
+++ OnTheMap/Model.swift
@@ -54,8 +54,8 @@ class Model : NSObject {
     }
     // MARK: Shared Instance
     func getStudentLocations(controller : UIViewController) {
-        
-        let request = NSMutableURLRequest(URL: NSURL(string: "https://api.parse.com/1/classes/StudentLocation")!)
+        let requestString = "https://api.parse.com/1/classes/StudentLocation?limit=100&order=-updatedAt"
+        let request = NSMutableURLRequest(URL: NSURL(string: requestString)!)
         request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
         request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
         let session = NSURLSession.sharedSession()
